dac102b9f456bb8a716c85db410ed9aa1f004dc6
DAVITE, Nicolás Alceo <92000544+NicoADavite@users.noreply.github.com>
2022-05-29 15:12:30 -0300

    Update autos.js

--- autos.js
+++ autos.js
@@ -8,18 +8,18 @@ let autos = [
         precio: 150000,
         cuotas: 12,
         patente: "APL123",
-        vendido: false,
+        vendido: false
     },
     {
-    marca: "Toyota",
-    modelo: "Corolla",
-    color: "Blanco",
-    anio: 2019,
-    km: 0,
-    precio: 100000,
-    cuotas: 14,
-    patente: "JJK116",
-    vendido: false,
+        marca: "Toyota",
+        modelo: "Corolla",
+        color: "Blanco",
+        anio: 2019,
+        km: 0,
+        precio: 100000,
+        cuotas: 14,
+        patente: "JJK116",
+        vendido: false
     }
 ];
 
